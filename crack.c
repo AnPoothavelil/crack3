1e2c7d4bf35f14e25fb13fada5c27aed455cf59a
AnPoothavelil <apoothavelil@student.sierracollege.edu>
2024-12-10 09:34:39 -0800

    Update crack.c

--- crack.c
+++ crack.c
@@ -2,67 +2,145 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "md5.h"
-
-#if __has_include("fileutil.h")
 #include "fileutil.h"
-#endif
 
-#define PASS_LEN 50     // Maximum length any password will be.
-#define HASH_LEN 33     // Length of hash plus one for null.
-
-
-int main(int argc, char *argv[])
+// Load the text file into a 2D array
+char (*loadFile2D(char *filename, int *size))[COLS]
 {
-    if (argc < 3) 
+    int CAPACITY = 10;
+    FILE *in = fopen(filename, "r");
+    if (!in)
     {
-        printf("Usage: %s hash_file dictionary_file\n", argv[0]);
+        perror("Can't open file");
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
-    int size;
-    int hc;
-    char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    char **hashes = loadFile(argv[1], &size);
-
-    FILE *givFile = fopen(argv[2], "r");
-
-    char ps[PASS_LEN];
-
-    while (fgets(password, PASS_LEN, givFile)) {
-        char *nPed = md5String(password); //gave up and copied from another code
-        for (int i = 0; i < size; i++) {
-            if (hashes[i] == nPed){
-                printf("tagged");
-                hc++;
-            }
-        }
-        free(nPed);
+    char (*arr)[COLS] = malloc(CAPACITY * sizeof(char[COLS]));
+    if (!arr)
+    {
+        perror("Memory allocation failed");
+        exit(1);
     }
 
-    fclose(givFile);
-    printf("Number of passwords found: %d\n", hc);
-    free(hashes);  // Free the array of hashes.
+    *size = 0;
+    char buffer[1000];
 
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    // Read the file line by line
+    while (fgets(buffer, sizeof(buffer), in))
+    {
+        // Trim newline
+        buffer[strcspn(buffer, "\n")] = '\0';
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+        // Expand the array if needed
+        if (*size >= CAPACITY)
+        {
+            CAPACITY *= 2;
+            arr = realloc(arr, CAPACITY * sizeof(char[COLS]));
+            if (!arr)
+            {
+                perror("Memory reallocation failed");
+                exit(1);
+            }
+        }
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+        // Copy the line into the array
+        strcpy(arr[*size], buffer);
+        (*size)++;
+    }
+
+    fclose(in);
+    return arr;
+}
+
+// Search the 2D array for a substring
+char *substringSearch2D(char *target, char (*lines)[COLS], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (strstr(lines[i], target))
+        {
+            return lines[i];
+        }
+    }
+    return NULL;
+}
+
+// Free memory used by the 2D array
+void free2D(char (*arr)[COLS])
+{
+    free(arr);
+}
+
+// Load text file into an array of arrays
+char **loadFileAA(char *filename, int *size)
+{
+    int CAPACITY = 10;
+    FILE *in = fopen(filename, "r");
+    if (!in)
+    {
+        perror("Can't open file");
+        exit(1);
+    }
+
+    char **arr = malloc(CAPACITY * sizeof(char *));
+    if (!arr)
+    {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+
+    *size = 0;
+    char buffer[1000];
+
+    // Read file line by line
+    while (fgets(buffer, sizeof(buffer), in))
+    {
+        buffer[strcspn(buffer, "\n")] = '\0';  // Trim newline
+
+        if (*size >= CAPACITY)
+        {
+            CAPACITY *= 2;
+            arr = realloc(arr, CAPACITY * sizeof(char *));
+            if (!arr)
+            {
+                perror("Memory reallocation failed");
+                exit(1);
+            }
+        }
+
+        arr[*size] = malloc(strlen(buffer) + 1);
+        if (!arr[*size])
+        {
+            perror("Memory allocation failed");
+            exit(1);
+        }
+        strcpy(arr[*size], buffer);
+        (*size)++;
+    }
+
+    fclose(in);
+    return arr;
+}
+
+// Search the array-of-arrays for a substring
+char *substringSearchAA(char *target, char **lines, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (strstr(lines[i], target))
+        {
+            return lines[i];
+        }
+    }
+    return NULL;
+}
+
+// Free memory used by the array of arrays
+void freeAA(char **arr, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        free(arr[i]);
+    }
+    free(arr);
 }
